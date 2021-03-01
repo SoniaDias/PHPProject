2a9aa918cb3dd30f5cdb415f5471019a3be63e58
SoniaDias <sonia.c.a.dias@gmail.com>
2021-03-01 21:59:31 +0000

    Day1

---
+++ 01_02_printing/hello.php
@@ -0,0 +1,40 @@
+<?php
+    echo 'Hello World!';
+?>
+
+<?php
+    print 'Hello World!';
+?>
+
+<?='Hello World!'?>
+<?php
+//sigle line
+/* multiline */
+
+//variables
+//store information to be used later
+?>
+<?php
+    //var
+    $age = 34;
+
+    //constant
+    define('VERSION', 1.1)
+    //echo VERSION;
+    //are immutable, cannot be change at runtime
+
+    /* PREDEFINED WORDS - RESERVED WORDS
+
+    */
+
+    /* types
+        integers
+        floats
+        characters
+        strings
+        booleans
+        --case sensitive
+        array
+    */
+?>
+
